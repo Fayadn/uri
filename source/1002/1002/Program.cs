2d451805b1f3d8cb76be19fd428a74221009337e
Fayad <fayadn@gmail.com>
2021-04-12 20:45:12 -0300

    Inicial

---
+++ source/1002/1002/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double AREA, π, RAIO;
+
+                π = 3.14159;
+            RAIO = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            AREA = π * Math.Pow(RAIO,2);
+
+            Console.WriteLine("A=" + AREA.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
