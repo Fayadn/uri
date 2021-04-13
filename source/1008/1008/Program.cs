b3f378e783997941c161211cb6dea7273f010df1
Fayad <fayadn@gmail.com>
2021-04-13 16:16:44 -0300

    Novos Exercicios

---
+++ source/1008/1008/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1008 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NH, NUMBER;
+            double VR, SALARY;
+
+            NUMBER = int.Parse(Console.ReadLine());
+            NH = int.Parse(Console.ReadLine());
+            VR = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            SALARY = NH * VR;
+
+            Console.WriteLine("NUMBER = " + NUMBER);
+            Console.WriteLine("SALARY = U$ " + SALARY.ToString("f2",CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
---
+++ source/1009/1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1009
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+                        double vendas, salario, sf;
+
+            nome = Console.ReadLine();
+            sf = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            vendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salario = sf + vendas * 0.15;
+
+            Console.WriteLine("TOTAL = R$ " + salario.ToString("f2",CultureInfo.InvariantCulture));
+        }
+    }
+}
