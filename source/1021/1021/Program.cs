a117518b4e2ed5b2f44706ce15e721e0ce3568df
Fayad <fayadn@gmail.com>
2021-04-14 20:55:16 -0300

    Desafios

--- source/1021/1021/Program.cs
+++ source/1021/1021/Program.cs
@@ -7,51 +7,77 @@ namespace _1021
     {
         static void Main(string[] args)
         {
-            double x, n100, n50, n20, n10, n5, n2, resto100, resto50, resto20, resto10, resto5, resto2,
-                m1, m50, m25, m10, m5, m01, rm1, rm50, rm25, rm10, rm5;
+            double N;
+            int qtd, resto, nota, moeda;
 
 
-            x = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            N = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            n100 = (int) x / 100;
-            resto100 = x % 100;
-            n50 = (int) resto100 / 50;
-            resto50 = resto100 % 50;
-            n20 = (int)resto50 / 20;
-            resto20 = resto50 % 20;
-            n10 = (int)resto20 / 10;
-            resto10 = resto20 % 10;
-            n5 = (int)resto10 / 5;
-            resto5 = resto10 % 5;
-            n2 = (int)resto5 / 2;
-            resto2 = resto5 % 2;
-
-            m1 = resto2 / 1;
-            rm1 = resto2 % 1;
-            m50 = rm1 / 0.50;
-            rm50 = rm1 % 0.50;
-            m25 = rm50 / 0.25;
-            rm25 = rm50 % 0.25;
-            m10 = rm25 / 0.10;
-            rm10 = rm25 % 0.10;
-            m5 = rm10 / 0.05;
-            rm5 = rm10 % 0.05;
-            m01 = rm5 / 0.01;
+            resto = (int)(N * 100 + 0.5);
 
             Console.WriteLine("NOTAS:");
-            Console.WriteLine(n100 + " nota(s) de R$ 100.00");
-            Console.WriteLine(n50 + " nota(s) de R$ 50.00");
-            Console.WriteLine(n20 + " nota(s) de R$ 20.00");
-            Console.WriteLine(n10 + " nota(s) de R$ 10.00");
-            Console.WriteLine(n5 + " nota(s) de R$ 5.00");
-            Console.WriteLine(n2 + " nota(s) de R$ 2.00");
+
+            nota = 100;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ "+ nota + ".00");
+            resto = resto % (nota * 100);
+
+            nota = 50;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ " + nota + ".00");
+            resto = resto % (nota * 100);
+
+            nota = 20;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ " + nota + ".00");
+            resto = resto % (nota * 100);
+
+            nota = 10;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ " + nota + ".00");
+            resto = resto % (nota * 100);
+
+            nota = 5;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ " + nota + ".00");
+            resto = resto % (nota * 100);
+
+            nota = 2;
+            qtd = resto / (nota * 100);
+            Console.WriteLine(qtd + " nota(s) de R$ " + nota + ".00");
+            resto = resto % (nota * 100);
+
             Console.WriteLine("MOEDAS:");
-            Console.WriteLine((int)m1 + " moeda (s) de R$ 1.00");
-            Console.WriteLine((int)m50 + " moeda (s) de R$ 0.50");
-            Console.WriteLine((int)m25 + " moeda (s) de R$ 0.25");
-            Console.WriteLine((int)m10 + " moeda (s) de R$ 0.10");
-            Console.WriteLine((int)m5 + " moeda (s) de R$ 0.05");
-            Console.WriteLine((int)m01 + " moeda (s) de R$ 0.01");
+
+            moeda = 100;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 1.00");
+            resto = resto % (moeda);
+
+            moeda = 50;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 0.50");
+            resto = resto % (moeda);
+
+            moeda = 25;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 0.25");
+            resto = resto % (moeda);
+
+            moeda = 10;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 0.10");
+            resto = resto % (moeda);
+
+            moeda = 5;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 0.05");
+            resto = resto % (moeda);
+
+            moeda = 1;
+            qtd = resto / (moeda);
+            Console.WriteLine(qtd + " moeda(s) de R$ 0.01");
+
 
         }
     }
---
+++ source/1061/1061/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _1061
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int inicio, termino, hi, mi, si, ht, mt, st, dia, hora, minuto, segundo, duracao;
+            string[] vet;
+
+           Console.Write("Dia "); inicio = int.Parse(Console.ReadLine());
+            vet = Console.ReadLine().Split(' ');
+            hi = int.Parse(vet[0]);
+            mi = int.Parse(vet[2]);
+            si = int.Parse(vet[4]);
+
+            Console.Write("Dia "); termino = int.Parse(Console.ReadLine());
+            vet = Console.ReadLine().Split(' ');
+            ht = int.Parse(vet[0]);
+            mt = int.Parse(vet[2]);
+            st = int.Parse(vet[4]);
+
+            inicio = inicio * 24 * 60 * 60 + hi * 60 * 60 + mi * 60 + si;
+            termino = termino * 24 * 60 * 60 + ht * 60 * 60 + mt * 60 + st;
+
+            duracao = termino - inicio;
+
+            dia = duracao / (24 * 60 * 60);
+            segundo = duracao % (24 * 60 * 60);
+            hora = segundo / (60 * 60);
+            segundo = segundo % (60 * 60);
+            minuto = segundo / 60;
+            segundo = segundo % 60;
+
+            Console.WriteLine(dia + " dia(s)");
+            Console.WriteLine(hora + " hora(s)");
+            Console.WriteLine(minuto + " minuto(s)");
+            Console.WriteLine(segundo + " segundo(s)");
+
+
+
+
+        }
+    }
+}
