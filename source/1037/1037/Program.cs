7fc3d9e59a2485d8635e6b801c7ac6054208439e
Fayad <fayadn@gmail.com>
2021-04-22 08:16:28 -0300

    Expressões Comparativas

---
+++ source/1037/1037/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1037
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double entrada = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (entrada < 0 || entrada > 100) {
+                Console.WriteLine("Fora de Intervalo");
+            }
+            else if (entrada <= 25.0) {
+                Console.WriteLine("Intervalo [0,25]");
+            }
+            else if (entrada <= 50.0) {
+                Console.WriteLine("Intervalo (25,50]");
+            }
+            else if (entrada <= 75.0) {
+                Console.WriteLine("Intervalo (50,75]");
+            }
+            else {
+                Console.WriteLine("Intervalo (75,100]");
+            }
+        }
+    }
+}
---
+++ source/1048/1048/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace _1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double percentual, reajuste, novoSalario;
+
+            if (salario <= 400.00)
+            {
+                percentual = 15.0;
+            }
+            else if (salario <= 800.00)
+            {
+                percentual = 12.0;
+            }
+            else if (salario <= 1200.00)
+            {
+                percentual = 10.0;
+            }
+            else if (salario <= 2000.00)
+            {
+                percentual = 7.0;
+            }
+            else
+            {
+
+                percentual = 4.0;
+            }
+
+            reajuste = salario * percentual / 100.0;
+            novoSalario = salario + reajuste;
+
+            Console.WriteLine("Novo Salario: " + novoSalario.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + reajuste.ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + percentual.ToString("f0", CultureInfo.InvariantCulture) + " %");
+
+        }
+    }
+}
